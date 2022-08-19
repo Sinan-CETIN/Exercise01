8ca4239ccf7b7589bf9d44813dd66cd36e334d48
sinan çetin <sinanctn54@gmail.com>
2022-08-19 14:49:45 +0300

    Answer of first seven questions.

---
+++ ProgramingExercises01/src/Exercises_01/Exercise_02.java
@@ -0,0 +1,12 @@
+package Exercises_01;
+
+public class Exercise_02 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Jaba!");
+	}
+
+}
---
+++ ProgramingExercises01/src/Exercises_01/Exercise_08.java
@@ -0,0 +1,10 @@
+package Exercises_01;
+
+public class Exercise_08 {
+	public static void main(String[] args) {
+		System.out.println("Radius is "+5.5);
+		System.out.println("Perimeter is "+(2 * 5.5 * Math.PI));
+		System.out.println("Area is "+(5.5 * 5.5 * Math.PI));
+	}
+
+}
